b4e59096e844ccb7105b68c60e3df0dafa6975dc
Paweł Perenc <pawelkperenc@gmail.com>
2021-03-23 07:11:15 +0100

    Objective calc

--- calculator.py
+++ calculator.py
@@ -1,125 +1,51 @@
-# Imports
 import sys
 if sys.version_info[0] == 2:  # Just checking your Python version to import Tkinter properly.
     from Tkinter import *
 else:
     from tkinter import *
 
-last_operation = 0
-current_operation = 0 # 1 add 2 dif 3 mul 4 div 5 percentage
-
-number_1 = 0
-number_2 = 0
-
-# Creating root
+import class_calculator
 
 root = Tk() 
 root.title("Calculator v.0.0.1")
 
-e = Entry(root, width = 35, borderwidth = 5) #width, fg, bg, borderwidth = 20
-e.grid(row = 0, column = 0, columnspan = 4, padx = 10, pady = 10)
-e.insert(0, "0")
+mainContainer = Frame(root)
+label = Label(mainContainer, text="")
 
-# Creating things
-#label_1 = Label(root, text = 'Hello world!')
-#label_2 = Label(root, text = "My name is Pawel Perenc")
+display = Entry(root, width = 35, borderwidth = 5) #width, fg, bg, borderwidth = 20
+display.grid(row = 0, column = 0, columnspan = 4, padx = 10, pady = 10)
+display.insert(0, "0")
 
-#button_1 = Button(root, text = 'Click me', padx = 50, pady = 20, command = myClick) # state = DISABLED, fg = "", bg = ""
-#button_1.pack()
+calc = class_calculator.Calculator(display)
 
-# Putting things on the screen
-#label_1.grid(row = 0, column = 0)
-#label_2.grid(row = 1, column = 0)
+def display_number():
+    root.after(100,display_number)
+    display.delete(0, END)
+    display.insert(0, calc.display_operator())
 
-# Functions
-
-def button_click(number):
-    current_string = e.get()
-    e.delete(0, END)
-    if current_string == '0':
-        e.insert(0, str(number))
-    else:
-        e.insert(0, str(current_string) + str(number))
-
-def set_operation(number):
-    global number_1
-    global current_operation
-    number_1 = float(e.get())
-    e.delete(0, END)
-    if number == 5:
-        result = number_1/100
-        e.insert(0, result)
-        current_operation = 0
-    else:
-        current_operation = number
-
-def equal():
-    global number_1
-    global number_2
-    global current_operation
-    global last_operation
-    result = 0
-    if current_operation == 0:
-        if last_operation == 1:
-            result = float(e.get()) + number_2
-        elif last_operation == 2:
-            result = float(e.get()) - number_2
-        elif last_operation == 3:
-            result = float(e.get()) * number_2
-        elif last_operation == 4:
-            result = float(e.get()) / number_2
-    else:
-        number_2 = float(e.get())
-        if current_operation == 1:
-            result = number_1 + number_2
-        elif current_operation == 2:
-            result = number_1 - number_2
-        elif current_operation == 3:
-            result = number_1 * number_2
-        elif current_operation == 4:
-            result = number_1 / number_2
-    e.delete(0, END)
-    e.insert(0, result)
-    if current_operation != 0:
-        last_operation = current_operation
-    current_operation = 0
-
-def backspace():
-    current_string = e.get()
-    e.delete(0, END)
-    e.insert(0, current_string[0:len(current_string)-1])
-    if e.get() == '':
-        e.insert(0, "0")
-
-def clear():
-    global number_1
-    global current_operation
-    e.delete(0, END)
-    e.insert(0, "0")
-    number_1 = 0
-    current_operation = 0
+display_number()
 
 button_x = 11
 button_y = 3
-button_1 = Button(root, text = '1', width = button_x, height = button_y, command = lambda: button_click(1))
-button_2 = Button(root, text = '2', width = button_x, height = button_y, command = lambda: button_click(2))
-button_3 = Button(root, text = '3', width = button_x, height = button_y, command = lambda: button_click(3))
-button_4 = Button(root, text = '4', width = button_x, height = button_y, command = lambda: button_click(4))
-button_5 = Button(root, text = '5', width = button_x, height = button_y, command = lambda: button_click(5))
-button_6 = Button(root, text = '6', width = button_x, height = button_y, command = lambda: button_click(6))
-button_7 = Button(root, text = '7', width = button_x, height = button_y, command = lambda: button_click(7))
-button_8 = Button(root, text = '8', width = button_x, height = button_y, command = lambda: button_click(8))
-button_9 = Button(root, text = '9', width = button_x, height = button_y, command = lambda: button_click(9))
-button_0 = Button(root, text = '0', width = button_x, height = button_y, command = lambda: button_click(0))
-button_dot = Button(root, text = '.', width = button_x, height = button_y, command = lambda: button_click('.'))
-button_percentage = Button(root, text = '%', width = button_x, height = button_y, command = lambda: set_operation(5))
-button_add = Button(root, text = '+', width = button_x, height = button_y, command = lambda: set_operation(1))
-button_dif = Button(root, text = '-', width = button_x, height = button_y, command = lambda: set_operation(2))
-button_mul = Button(root, text = '*', width = button_x, height = button_y, command = lambda: set_operation(3))
-button_div = Button(root, text = '/', width = button_x, height = button_y, command = lambda: set_operation(4))
-button_equal = Button(root, text = '=', width = 25, height = button_y, command = equal)
-button_backspace = Button(root, text = '<--', width = 11, height = button_y, command = backspace)
-button_clear = Button(root, text = 'clear', width = 11, height = button_y, command = clear)
+button_1 = Button(root, text = '1', width = button_x, height = button_y, command = lambda: calc.button_click(1))
+button_2 = Button(root, text = '2', width = button_x, height = button_y, command = lambda: calc.button_click(2))
+button_3 = Button(root, text = '3', width = button_x, height = button_y, command = lambda: calc.button_click(3))
+button_4 = Button(root, text = '4', width = button_x, height = button_y, command = lambda: calc.button_click(4))
+button_5 = Button(root, text = '5', width = button_x, height = button_y, command = lambda: calc.button_click(5))
+button_6 = Button(root, text = '6', width = button_x, height = button_y, command = lambda: calc.button_click(6))
+button_7 = Button(root, text = '7', width = button_x, height = button_y, command = lambda: calc.button_click(7))
+button_8 = Button(root, text = '8', width = button_x, height = button_y, command = lambda: calc.button_click(8))
+button_9 = Button(root, text = '9', width = button_x, height = button_y, command = lambda: calc.button_click(9))
+button_0 = Button(root, text = '0', width = button_x, height = button_y, command = lambda: calc.button_click(0))
+button_dot = Button(root, text = '.', width = button_x, height = button_y, command = lambda: calc.button_click('.'))
+button_percentage = Button(root, text = '%', width = button_x, height = button_y, command = lambda: calc.set_operation(5))
+button_add = Button(root, text = '+', width = button_x, height = button_y, command = lambda: calc.set_operation(1))
+button_dif = Button(root, text = '-', width = button_x, height = button_y, command = lambda: calc.set_operation(2))
+button_mul = Button(root, text = '*', width = button_x, height = button_y, command = lambda: calc.set_operation(3))
+button_div = Button(root, text = '/', width = button_x, height = button_y, command = lambda: calc.set_operation(4))
+button_equal = Button(root, text = '=', width = 25, height = button_y, command = calc.equal)
+button_backspace = Button(root, text = '<--', width = 11, height = button_y, command = calc.backspace)
+button_clear = Button(root, text = 'clear', width = 11, height = button_y, command = calc.clear)
 
 button_1.grid(row=3, column = 0)
 button_2.grid(row=3, column = 1)
@@ -151,4 +77,3 @@ button_clear.grid(row=5, column = 3)
 root.bind('<F11>', lambda event: root.attributes('-fullscreen', not root.attributes("-fullscreen")))
 
 root.mainloop()
-
---
+++ class_calculator.py
@@ -0,0 +1,88 @@
+import sys
+if sys.version_info[0] == 2:  # Just checking your Python version to import Tkinter properly.
+    from Tkinter import *
+else:
+    from tkinter import *
+
+class Calculator():
+
+    number1 = 'a'
+    number2 = 'a'
+    memory = 0
+
+    # 1 add 2 dif 3 mul 4 div 5 percentage
+    operation = 0 
+
+    current_string = ''
+
+    def __init__(self, display):
+        self.display = display
+
+    def button_click(self, button):
+        if not('.' in self.current_string and button == '.'):
+            if self.current_string == '0':
+                self.current_string = str(button)
+            else:
+                self.current_string += str(button)
+            self.display_operator()
+
+    def set_operation(self, operation_id):
+        if self.current_string != '':
+            self.save_numbers('set')
+            self.operation = operation_id
+            self.current_string = ''
+            if operation_id == 5:
+                self.equal()
+            self.display_operator()
+
+    def equal(self):
+        self.save_numbers('equal')
+        if isinstance(self.number1, str) or isinstance(self.number2, str):
+            self.number2 = self.memory
+        result = 0
+        if self.operation == 1:
+            result = self.number1 + self.number2
+        elif self.operation == 2:
+            result = self.number1 - self.number2
+        elif self.operation == 3:
+            result = self.number1 * self.number2
+        elif self.operation == 4:
+            result = self.number1 / self.number2
+        elif self.operation == 5:
+            result = self.number1/100
+
+        if not isinstance(self.number1, str) and not isinstance(self.number2, str):
+            self.number1 = 'a'
+            self.number2 = 'a'
+
+        self.current_string = str(result)
+        self.display_operator()
+
+    def backspace(self):
+        if len(self.current_string)>1:
+            self.current_string = self.current_string[:-1]
+        else:
+            self.current_string = ''
+        self.display_operator()
+
+    def clear(self):
+        self.number1 = 'a'
+        self.number2 = 'a'
+        self.current_string = ''
+        self.operation = 0
+        self.display_operator()
+
+    def save_numbers(self, method):
+        try:
+            if self.number1 == 'a':
+                self.number1 = float(self.current_string)
+            elif self.number2 == 'a':
+                self.number2 = float(self.current_string)
+                self.memory = self.number2
+            elif not isinstance(self.number1, str) and not isinstance(self.number2, str) and method == 'equal':
+                self.number1 = float(self.current_string)
+        except:
+            pass
+
+    def display_operator(self):
+        return self.current_string
